f6dcddd451b76c3b54b9826cbf2a11e69a44d08c
tiwavaldese <tiwaval7@gmail.com>
2023-04-12 19:46:26 +0100

    phoneNumValid.js

---
+++ phoneNumValid.js
@@ -0,0 +1,9 @@
+alert
+function telephoneCheck(str) {
+    let regex = /^1?\s?(\d{3}|\(\d{3}\))-?\s?\d{3}-?\s?\d{4}$/gm
+    // /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/
+    
+    return regex.test(str);
+  }
+  
+  console.log(telephoneCheck("555-555-5555"));
